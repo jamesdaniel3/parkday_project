e43385282a1b93d3dbae3b799df36cc29a6e0096
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-03-07 19:03:35 -0400

    createed base menu card file

---
+++ parkday-frontend/components/MenuCard.tsx
@@ -0,0 +1,15 @@
+import { MenuCardProps } from "@/types/types";
+
+const MenuCard: React.FC<MenuCardProps> = ({
+  id,
+  name,
+  isVegetarian,
+  isKeto,
+  isVegan,
+  isDairyFree,
+  isPaleo,
+  description,
+  image_url,
+}) => {
+  return <>This is a menu card</>;
+};
